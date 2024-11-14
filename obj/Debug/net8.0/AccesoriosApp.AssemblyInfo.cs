068bb05db9ae7b73b944fe82363d3bdd597d6140
Nycolly-Mendez <mistshellym@gmail.com>
2024-11-13 19:38:38 -0600

    Función para editar tipo de accesorios

--- obj/Debug/net8.0/AccesoriosApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/AccesoriosApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccesoriosApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f917d949d40bb358bdde5aee87da01b28b0aed14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9451098857b3c51d3d6c223c3ea191b422265b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccesoriosApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccesoriosApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
