5014cac83ffe6c46b006895a71d035e5555450b5
YeniferCenteno <yenifercentenogr@gmail.com>
2024-11-20 22:50:05 -0600

    all of category types

--- DTOs/TipoDeAccesorioDTO.cs
+++ DTOs/TipoDeAccesorioDTO.cs
@@ -3,5 +3,23 @@
     public class TipoDeAccesorioDTO
     {
         public string Nombre { get; set; } = null!;
+
     }
+
+    public class TipoDeAccesorioEditar
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+
+    public class ContentResponseTipoAccesorio
+    {
+        public List<TipoDeAccesorio> Content { get; set; }
+        public Pageable Pageable { get; set; } 
+        public int TotalElements { get; set; }
+        public int TotalPages { get; set; }
+        public bool Last { get; set; }
+        public bool First { get; set; }
+    }
+
 }
